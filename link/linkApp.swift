59c084726f7856466d0b726660f5f0e7a06edb53
Liz Cruickshank <liz.milkshake@gmail.com>
2024-06-18 17:28:55 -0400

    Initial Commit

---
+++ link/linkApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkApp.swift
+//  link
+//
+//  Created by Liz Cruickshank on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct linkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
